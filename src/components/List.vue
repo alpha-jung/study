38a3cf575fc6093defd6fff57b292d124ea99065
alpha-jung <shter7119@gmail.com>
2023-06-22 18:31:43 +0900

    vue blog

---
+++ src/components/List.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-for="(blog, i) in blogData" :key="i">
+        <!-- <h3>
+            <router-link :to="{ path: '/detail/' + i }">{{ blog.title }}</router-link>
+        </h3> -->
+        <h3 @click="$router.push('/detail/' + i)">{{ blog.title }}</h3>
+        <p>{{ blog.content }}</p>
+        <p>{{ blog.date }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'List',
+    props: {
+        blogData: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
